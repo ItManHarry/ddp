5be6a15ea5795d1e01884ecddbac0dc186b50710
itmanharry <jack_chengqian@163.com>
2021-01-11 10:49:46 +0800

    program invoice

--- src/main/groovy/com/doosan/ddp/pm/dao/domain/biz/pro/ProgramInvoice.groovy
+++ src/main/groovy/com/doosan/ddp/pm/dao/domain/biz/pro/ProgramInvoice.groovy
@@ -2,6 +2,8 @@ package com.doosan.ddp.pm.dao.domain.biz.pro
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Table
+import javax.persistence.Transient
+
 import com.doosan.ddp.pm.dao.domain.base.TableEntityBaseModel
 /**
  * 项目发票信息
@@ -12,9 +14,12 @@ class ProgramInvoice extends TableEntityBaseModel {
 	//所属项目(项目uuid)
 	@Column(name="programid", length=40)
 	String programid
-	//区分（1：首付款 2：中期款 3：尾款）
+	//区分代码（1：首付款 2：中期款 3：尾款）
 	@Column(name="stage")
 	int stage
+	//区分显示
+	@Transient
+	String stageStr
 	//支付比例
 	@Column(name="percent")
 	int percent
